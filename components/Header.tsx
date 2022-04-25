9209ff53f192c9156c9009e1497ea7e073021521
Raphael Sales <raphael.sales@vinted.com>
2022-04-25 12:16:33 +0200

    Updating libraries to latest versions and adding callback examples

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,7 @@
-import { signIn, signOut, useSession } from 'next-auth/client'
+import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Header() {
-
-  const [session, loading] = useSession();
+  const { data: session } = useSession()
 
   let button;
 
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,16 +1,29 @@
 import NextAuth from 'next-auth'
-import Providers from 'next-auth/providers'
+import OktaProvider from 'next-auth/providers/okta'
 
 const options = {
   // Configure one or more authentication providers
   providers: [
-    Providers.Okta({
+    OktaProvider({
       clientId: process.env.OKTA_CLIENTID,
       clientSecret: process.env.OKTA_CLIENTSECRET,
-      domain: process.env.OKTA_DOMAIN
+      issuer: process.env.OKTA_ISSUER
     }),
     // ...add more providers here
-  ]
+  ],
+  callbacks: {
+    async session({ session, user, token }) {
+      session.accessToken = token.accessToken
+      return session
+    },
+    async jwt({ token, user, account, profile, isNewUser }) {
+      // Persist the OAuth access_token to the token right after signin
+      if (account) {
+        token.accessToken = account.access_token
+      }
+      return token
+    }
+  }
 }
 
 export default (req, res) => NextAuth(req, res, options)
